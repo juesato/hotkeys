e999de2f260b7860937839bb9530e61ea1a70451
Jonathan Uesato <juesato@gmail.com>
2020-04-01 23:11:03 +0100

    update bringToFrontCycle.py for py3 compatibility

--- bringToFrontCycle.py
+++ bringToFrontCycle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
-import commands
+# import commands
 
+import subprocess
 from subprocess import call
 
+def getoutput(*args, **kwargs):
+    return subprocess.getoutput(*args, **kwargs)
+    # return commands.getoutput(*args, **kwargs)
 
 program_name = sys.argv[1] # the program to be focused
 program_call = sys.argv[2] # the command to start program if not already open
@@ -12,7 +16,7 @@ program_call = sys.argv[2] # the command to start program if not already open
 # get all windows which contain "program_name" from wmcontrol
 # candidates = sorted([x.strip() for x in commands.getoutput(""" wmctrl -l -x | awk -v win="%s" 'tolower($0) ~ win {print $1;}' """ % (program_name, )).split("\n") if x !=''])
 candidates = []
-outputs = commands.getoutput("wmctrl -l -x")
+outputs = getoutput("wmctrl -l -x")
 for line in outputs.split('\n'):
     if program_name in line:
         candidates.append(line.split(' ')[0])
@@ -29,7 +33,7 @@ if candidates : # at least one candidate found , we need to check if the active 
         #       does not do the padding and might give a window id starting with 0x followed by only 6 digits. The
         #       lines below get the id of the current window and make the id returned by xprop comaptible with
         #       the window ids returned by wmctrl.
-        active_window_string = commands.getoutput("""xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)" """)
+        active_window_string = getoutput("""xprop -root | grep "_NET_ACTIVE_WINDOW(WINDOW)" """)
         active_window_string =  active_window_string[active_window_string.find("#")+4:].strip()
         active_window = "0x" + "0" * (8-len(active_window_string)) + active_window_string
  
@@ -43,5 +47,5 @@ if candidates : # at least one candidate found , we need to check if the active 
                 os.system(""" wmctrl -i -a "%s" """ % (next_window,) ) # tell wmcontrol to display the next_window
 else: # no windows open which fit the pattern of program_name
     # os.system("%s &" % (program_name,)) # open new window
-    pass
-    # call(program_call)
+    # pass
+    call(program_call)
